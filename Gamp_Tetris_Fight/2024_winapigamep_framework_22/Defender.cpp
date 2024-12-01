18bfe091505b1d66a4e49e3e073465a63fb43f4b
reum <ahreun0408@gmail.com>
2024-12-01 23:44:50 +0900

    fixed | 점프 수정, 이제 이동 가능

--- Gamp_Tetris_Fight/2024_winapigamep_framework_22/Defender.cpp
+++ Gamp_Tetris_Fight/2024_winapigamep_framework_22/Defender.cpp
@@ -9,7 +9,7 @@
 #include "JumpState.h"
 #include "FallState.h"
 
-Defender::Defender(): m_stateMachine(new StateMachine<Defender>(this)), m_jumpCount(0), m_maxJumpCount(4), m_verticalSpeed(0.0f), m_isGrounded(false){
+Defender::Defender(): m_stateMachine(new StateMachine<Defender>(this)), m_jumpCount(0), m_maxJumpCount(4), m_isGrounded(false){
     m_stateMachine->ChangeState(new IdleState());
     m_collider->SetID(0);
 }
@@ -24,26 +24,13 @@ void Defender::UseSkill()
 		cout << "defender use skill" << endl;
 	}
 }
-//void Defender::ApplyGravity() {
-//    if (m_currentState != DefenderState::Idle) {
-//        m_verticalSpeed -= m_gravity * fDT; // 중력 적용
-//        Vec2 pos = GetPos();
-//        pos.y -= m_verticalSpeed * fDT;
-//        SetPos(pos);
-//    }
-//}
 
 void Defender::Update() {
-    //Player::Update();
+    Player::Update();
     m_stateMachine->Update();
 
     Vec2 vPos = GetPos();
-    // 점프 (지면에 있을 때만)
-    if (m_isGrounded && GET_KEYDOWN(KEY_TYPE::SPACE))
-    {
-        m_vVelocity.y = -m_jumpPower; // 위쪽으로 힘을 가함
-        m_isGrounded = false;         // 점프 상태로 전환
-    }
+    m_beforePos = vPos;
 
     // 중력 적용
     if (!m_isGrounded)
@@ -61,22 +48,12 @@ void Defender::Update() {
     }
     SetPos(vPos);
 }
-//void Defender::Jump() {
-//    if (m_currentJumpCount < m_maxJumpCount) {
-//        m_verticalSpeed = m_jumpPower; // 점프 속도 설정
-//        m_currentState = DefenderState::Jump;        // 상태를 Jump로 변경
-//        m_currentJumpCount++;
-//        cout << "Defender jumps! Jump count: " << m_currentJumpCount << endl;
-//    }
-//}
 
 void Defender::EnterCollision(Collider* _other)
 {
     if (IsGround(m_collider, _other)) {
         m_isGrounded = true;
-        //Vec2 vPos = GetPos();
-        //vPos.y = _other->GetLatedUpatedPos().y - m_collider->GetSize().y / 2; // 지면 위에 위치 고정
-        //SetPos(vPos);
+        m_vVelocity.y = 0; // 중력 가속도를 초기화
     }
 }
 
@@ -96,22 +73,14 @@ void Defender::ExitCollision(Collider* _other)
 }
 void Defender::PerformJump() {
     if (m_jumpCount < m_maxJumpCount) {
+         m_isGrounded = false;         // 점프 상태로 전환
         m_jumpCount++;
-        m_verticalSpeed = 300.0f; // 예시 점프 속도
-        std::cout << "Jump performed! Jump count: " << m_jumpCount << std::endl;
+
+        Vec2 vPos = GetPos();
+        m_vVelocity.y = -m_jumpPower; // 위쪽으로 힘을 가함
+        vPos.y += m_vVelocity.y * fDT;
+        SetPos(vPos);
     }
-}
-
-bool Defender::IsGrounded() const {
-    return m_isGrounded;
-}
-
-bool Defender::IsJumpKeyPressed() const {
-    return GET_KEYDOWN(KEY_TYPE::SPACE);
-}
-
-bool Defender::IsFalling() const {
-    return m_verticalSpeed < 0;
 }
 bool Defender::IsGround(Collider* self, Collider* other)
 {
--- Gamp_Tetris_Fight/2024_winapigamep_framework_22/Defender.h
+++ Gamp_Tetris_Fight/2024_winapigamep_framework_22/Defender.h
@@ -18,12 +18,10 @@ public:
 	// 상태 관리
 	StateMachine<Defender>* GetStateMachine() const { return m_stateMachine; }
 	void PerformJump();
-	bool IsGrounded() const;
-	bool IsJumpKeyPressed() const;
-	bool IsFalling() const;
+	bool IsGrounded() const { return m_isGrounded; }
+	bool IsJumpKeyPressed() const { return GET_KEYDOWN(KEY_TYPE::SPACE); }
+	bool IsFalling() const { return !m_isGrounded && GetPos().y < m_beforePos.y; }
 private:
-	//void Jump();
-	//void ApplyGravity();
 	bool IsGround(Collider* self, Collider* other);
 private:
 	enum class DefenderState {
@@ -34,10 +32,11 @@ private:
 	StateMachine<Defender>* m_stateMachine;
 	int m_jumpCount;
 	const int m_maxJumpCount;
-	float m_verticalSpeed;
 
 	bool m_isGrounded;
 	const float m_jumpPower = 300.f;
 	const float m_gravity = 980.f;
 	Vec2 m_vVelocity;
+
+	Vec2 m_beforePos;
 };
